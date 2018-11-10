b8bde40ded395fba8bf495017c0c684c9dacdaa9
Sunny Udhani <sunny.iv09@gmail.com>
2018-11-09 19:06:00 -0800

    update file names

--- src/knn_classification_p1.py
+++ src/knn_classification_p1.py
@@ -1,7 +1,15 @@
 
 # coding: utf-8
 
-# In[183]:
+# ### Medical abstracts describe the current conditions of a patient. Doctors routinely scan dozens or hundreds of abstracts each day as they do their rounds in a hospital and must quickly pick up on the salient information pointing to the patient’s malady. You are trying to design assistive technology that can identify, with high precision, the class of problems described in the abstract. In the given dataset, abstracts from 5 different conditions have been included: digestive system diseases, cardiovascular diseases, neoplasms, nervous system diseases, and general pathological conditions.
+# 
+# * Choose appropriate techniques for modeling text.
+# * Implement the k-nearest neighbor classifier (cannot use libraries for this algorithm).
+# * Use your version of the k-NN classifier to assign classes to medical texts.
+# * Think about dealing with imbalanced data.
+# * Evaluate results using the F1 Scoring Metric.
+
+# In[1]:
 
 
 import numpy as np
@@ -19,8 +27,10 @@ from nltk.stem import WordNetLemmatizer
 from imblearn.over_sampling import SMOTE
 
 
-# In[184]:
+# In[2]:
 
+
+# utility to read data as panda dataframe
 
 def give_me_a_frame(data_location):
     return pd.read_csv(
@@ -29,34 +39,37 @@ def give_me_a_frame(data_location):
         sep='\n')
 
 
-# In[185]:
+# In[3]:
 
+
+# utility to perform preprocessing tests
 
 def preprocess(statements):
-#     print(statement)
+
     s = set(stopwords.words('english'))
     stemmer = WordNetLemmatizer()
     table = str.maketrans('', '', string.punctuation)
     digit_table = str.maketrans('', '', string.digits)
     
-    kal = []
+    result = []
     for statement in statements:
-#         print(statement)
+#       after lowercasing the sentence, remove digits, stopwords and lematization
+        
         statement = statement.lower()
-        aaj = list(filter(lambda w: not w in s,statement.split()))
-        stripped = [w.translate(table) for w in aaj]
-        stripped = [w.translate(digit_table) for w in stripped]
-        stripped = [d for d in stripped if len(d) > 1]
+        temp = list(filter(lambda w: not w in s,statement.split()))
+        stripped_punc_fix = [w.translate(table) for w in temp]
+        stripped_digit_fix = [w.translate(digit_table) for w in stripped_punc_fix]
+        stripped_wordlen_fix = [d for d in stripped_digit_fix if len(d) > 1]
+        stripped_stemmer_fix = [stemmer.lemmatize(stemmer_word) for stemmer_word in stripped_wordlen_fix]
+        result.append(" ".join(stripped_stemmer_fix))
 
-        stripped_stemmer_fix = [stemmer.lemmatize(stemmer_word) for stemmer_word in stripped]
-        kal.append(" ".join(stripped_stemmer_fix))
-#     print(stripped)
-#     print(kal[:2])
-    return kal
+    return result
 
 
-# In[186]:
+# In[4]:
 
+
+# utility to transform a bag of words to csr-matrix
 
 def build_matrix(train, test, n):
     vectorizer = TfidfVectorizer(norm='l2'
@@ -66,25 +79,29 @@ def build_matrix(train, test, n):
     return vectorizer.fit_transform(train),vectorizer.transform(test)
 
 
-# In[187]:
+# In[5]:
 
+
+# utility to predict output class of a single input object using k-nearest neighbors
 
 def predict_output(train_matrix, test_vector, train_classes,  k=3 ):
     dot_product = test_vector.dot(train_matrix.T)
     sims = list(zip(dot_product.indices, dot_product.data))
     sims.sort(key=lambda x: x[1], reverse=True)
+    
+#     take majority vote instead of using inverse similarity, decreased performance
 #     tc = Counter(train_classes[s[0]] for s in sims[:k]).most_common()
 #     if len(tc) < 2 or tc[0][1] > tc[1][1]:
 #             # majority vote
 #             return tc[0][0]
-#     print(train_classes[4863:4867])
+
     tc = defaultdict(float)
     for s in sims[:k]:
                 tc[train_classes[s[0]]] += s[1]
     return sorted(tc.items(), key=lambda x: x[1], reverse=True)[0][0]
 
 
-# In[174]:
+# In[6]:
 
 
 def splitData(mat, cls, fold=1, d=10):
@@ -110,46 +127,45 @@ def splitData(mat, cls, fold=1, d=10):
     return train, clstr
 
 
-# In[175]:
+# In[7]:
 
+
+# peform steps on the train data to view evaluation
 
 def classify(statements, statements_test, classes, c, k):
-    print("c = ", c)
-    print("k = ", k)
+    
     statements = preprocess(statements)
     statements_test = preprocess(statements_test)
     classes = np.array(classes)
     X_Train, X_test = build_matrix(statements,statements_test,c)
+    
+#   fix unbalanced data set
     nm = SMOTE(random_state=21)
     X_res, y_res = nm.fit_sample(X_Train, classes)
 
     X_train, X_test, y_train, y_test = train_test_split(X_res, y_res, test_size=0.33, random_state=21)
     y_pred = [predict_output(X_train,x,y_train,k) for x in X_test]
-    print(precision_recall_fscore_support(y_test, y_pred, average='macro'))    
-    
+    print(precision_recall_fscore_support(y_test, y_pred, average='macro'))
 
 
-# In[188]:
+# ### The goal is to develop predictive models that can determine, given a particular medical abstract, which one of 5 classes it belongs to. As such, the goal would be to develop the best classification model, with the restriction that only use your own implementation of the k-NN classifier. There are many choices in text pre-processing and modeling, proximity measures, and classifier meta-parameters that will lead to many different solutions for the problem.
+
+# In[12]:
 
 
-df = give_me_a_frame('train.dat')   
+df = give_me_a_frame('../data/train.dat')   
 df = pd.DataFrame(df[0].str.split('\t', 1).tolist())
 statements = df[1]
 classes = df[0]
-print(statements[0])
 
 # n, bins, patches = plt.hist(classes)
 # plt.show()
 
-df = give_me_a_frame('test.dat')   
+df = give_me_a_frame('../data/test.dat')
 statements_test = df[0]
-# print(statements_test)
-# df = pd.DataFrame({'aaj':statements})
-
-# df[df['aaj'].str.contains('acute')]
 
 
-# In[189]:
+# In[13]:
 
 
 statements = preprocess(statements)
@@ -164,7 +180,7 @@ print(statements[:1])
 X_Train, X_test = build_matrix(statements,statements_test,2)
 
 
-# In[190]:
+# In[14]:
 
 
 nm = SMOTE(random_state=21)
@@ -207,10 +223,12 @@ with open('out.dat', 'w') as f:
 # print(kal)
 
 
-# In[182]:
+# ### Evaluate training data
+
+# In[ ]:
 
 
-# for c in range(2,3):
-#     for k in range(73,74):
-#         classify(statements, statements_test, classes, c, k)
+for c in range(2,3):
+    for k in range(73,74):
+        classify(statements, statements_test, classes, c, k)
 
